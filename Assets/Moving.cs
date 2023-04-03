7476c80bd0ed557e057629545f462e1150ced868
Nefrol <119403760+Nefr0l@users.noreply.github.com>
2023-04-03 18:14:59 +0200

    Wersja 1.1
    - większy tekst w menu
    - poprawki w kodzie

--- Assets/Moving.cs
+++ Assets/Moving.cs
@@ -1,84 +1,68 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-    // Zmienne
-    public float inputSpeed = 10f;
-    public float rotateSpeed = 100f;
-    public float brakingSpeed = 2f;
-    public float maxSpeed = 20f;
+    public float inputSpeed;
+    public float rotateSpeed;
+    public float brakingSpeed;
+    public float maxSpeed;
+    public AudioClip hitSound;
     
     private Rigidbody2D rb; 
     private bool isMoving; 
-    private Camera mainCamera;  
-    public static float cameraHeight;    
-    public static float cameraWidth;
-    private float halfPlayerSize;
-
-    public AudioClip hitClip;
+    private Camera camera;  
+    private float cameraHeight;    
+    private float cameraWidth;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        mainCamera = Camera.main;
-        cameraHeight = mainCamera.orthographicSize - 0.5f;
-        cameraWidth = cameraHeight * mainCamera.aspect + 0.5f;
+        camera = Camera.main;
+        cameraHeight = camera.orthographicSize - 0.5f;
+        cameraWidth = cameraHeight * camera.aspect + 0.5f;
     }
 
     void Update()
     {
-        // Obrót obiektu
-        float rotationInput = Input.GetAxisRaw("Horizontal");
-        transform.Rotate(0, 0, -rotationInput * rotateSpeed * Time.deltaTime);
-
-        // Poruszanie obiektu
+        /*float rotationInput = Input.GetAxisRaw("Horizontal");
         float thrustInput = Input.GetAxisRaw("Vertical");
 
-        // Przyśpieszenie i hamowanie
+        transform.Rotate(0, 0, -rotationInput * rotateSpeed * Time.deltaTime);
+        rb.AddForce(transform.up * (thrustInput * inputSpeed));
+        rb.velocity = rb.velocity.normalized * Mathf.Max(rb.velocity.magnitude - brakingSpeed * Time.deltaTime, 0);
+        rb.velocity = rb.velocity.normalized * Mathf.Min(rb.velocity.magnitude, maxSpeed);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -cameraWidth, cameraWidth), Mathf.Clamp(transform.position.y, -cameraHeight, cameraHeight), transform.position.z);*/
+        
+        float rotationInput = Input.GetAxisRaw("Horizontal");
+        float thrustInput = Input.GetAxisRaw("Vertical");
+        
+        transform.Rotate(0, 0, -rotationInput * rotateSpeed * Time.deltaTime);
         if (thrustInput != 0)
         {
             rb.AddForce(transform.up * (thrustInput * inputSpeed));
             isMoving = true;
-        }
+        } 
         else if (isMoving)
         {
             rb.velocity = rb.velocity.normalized * Mathf.Max(rb.velocity.magnitude - brakingSpeed * Time.deltaTime, 0);
             isMoving = rb.velocity.magnitude > 0;
         }
-        
-        // Limit prędkości
+
         if (rb.velocity.magnitude > maxSpeed)
         {
             rb.velocity = rb.velocity.normalized * maxSpeed;
         }
-        
-        // Limit poruszania się gracza do wewnątrz ekranu
-        Vector3 newPosition = transform.position;
-        if (transform.position.y > cameraHeight)
-        {
-            newPosition.y = cameraHeight;
-        }
-        else if (transform.position.y < -cameraHeight)
-        {
-            newPosition.y = -cameraHeight;
-        }
-        if (transform.position.x > cameraWidth)
-        {
-            newPosition.x = cameraWidth;
-        }
-        else if (transform.position.x < -cameraWidth)
-        {
-            newPosition.x = -cameraWidth;
-        }
+
+        float newX = Mathf.Clamp(transform.position.x, -cameraWidth, cameraWidth); // Ustala zmienną na wartość bliższą ujemnej lub dodatniej szerokości kamery
+        float newY = Mathf.Clamp(transform.position.y, -cameraHeight, cameraHeight);
+        Vector3 newPosition = new Vector3(newX, newY, transform.position.z);
         transform.position = newPosition;
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         Manager.hp -= 1;
-        GameObject.Find("audio").GetComponent<AudioSource>().PlayOneShot(hitClip);
+        GameObject.Find("audio").GetComponent<AudioSource>().PlayOneShot(hitSound);
         Destroy(col.gameObject);
     }
 }
